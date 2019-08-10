5f4dcf6684152ea58d6f5fd7505ff99624c6c3e5
noonespecial009 <noonespecial009@gmail.com>
2019-08-10 00:26:57 -0700

    change map type to street-satellite

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox.emerald',
+        id: 'mapbox.streets-satellite',
         accessToken: 'pk.eyJ1Ijoib290aGViYWxsc29vIiwiYSI6ImNqeXBkMmN0MTA3OTUzbXFyMTc4MDJ5angifQ.mY2js0Eh5AWkpOejtzvecg'
     }).addTo(mymap);
 
