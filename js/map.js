bc61140b8d3bfcc4a5f078ada069b655c1922e35
noonespecial009 <noonespecial009@gmail.com>
2019-08-10 00:07:28 -0700

    center map on uvic

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 console.log("MAP")
 
 $(document).ready(function(){
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
+    var mymap = L.map('map').setView([48.460, -123.3117], 15);
     
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
