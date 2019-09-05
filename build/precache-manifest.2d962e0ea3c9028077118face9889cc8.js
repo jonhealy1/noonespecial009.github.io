7fabd8a976d9d05423114f62817c3afead6e7c5d
noonespecial009 <noonespecial009@gmail.com>
2019-09-05 10:43:19 -0700

    import build

---
+++ build/precache-manifest.2d962e0ea3c9028077118face9889cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0552aba54db44c4216938bc0f02ecf10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9ab55bdd4c33a7ed59",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0c355edcf6700ecb2ff2",
+    "url": "/static/js/2.a37bdac5.chunk.js"
+  },
+  {
+    "revision": "ed9ab55bdd4c33a7ed59",
+    "url": "/static/js/main.cc206cef.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  }
+]);
--- portfolio/src/resumeData.js
+++
@@ -1,6 +0,0 @@
-let resumeData = {
-    "imagebaseurl":"http://localhost:3000/",
-    "name": "Jonathan Healy",
-    "role": "Blockchain Developer"
-    }
-  export default resumeData
